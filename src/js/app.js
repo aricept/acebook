9041828dea4f13c481b33571ad5f7718d3cd1a16
Nathan Miller <aricept@gmail.com>
2015-07-25 15:35:14 -0500

    Complete text editing functionality

--- src/js/app.js
+++ src/js/app.js
@@ -10,3 +10,60 @@ document.addEventListener('DOMContentLoaded', function(){
   };
   bg.src = 'img/acebook - template.png';
 });
+
+var Kudos = function() {
+  var self = this;
+
+  self.name = ko.observable('Agent Name');
+  self.img = ko.observable('');
+  self.quote = ko.observable('A quote from the customer');
+  self.mgr = ko.observable('Manager Name');
+  self.pat = ko.observable('Commentary goes here');
+  self.likes = ko.observable(51);
+  self.editName = ko.observable(false);
+  self.editQuote = ko.observable(false);
+  self.editMgr = ko.observable(false);
+  self.editPat = ko.observable(false);
+
+};
+
+var acebook = function() {
+  var self = this;
+
+  self.replist = ko.observableArray();
+
+  self.replist().push(new Kudos());
+
+  self.editName = function(kudo) {
+    kudo.editName(!kudo.editName());
+  };
+
+  self.editMgr = function(kudo) {
+    kudo.editMgr(!kudo.editMgr());
+  };
+
+  self.editPat = function(kudo) {
+    kudo.editPat(!kudo.editPat());
+  };
+
+ self.editQuote = function(kudo) {
+    kudo.editQuote(!kudo.editQuote());
+  };
+
+  self.newKudo = function() {
+    self.replist.push(new Kudos());
+    console.log(self.replist().length, self.replist().indexOf(this));
+  };
+
+  self.likeClick = function(kudo) {
+    kudo.likes(kudo.likes() + 1);
+  };
+
+  self.selectImg = function() {
+    var file = document.getElementById('file');
+    file.click();
+  };
+
+};
+
+ko.applyBindings(new acebook());
